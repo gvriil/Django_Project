45b78d6279b0bb1ad1f29d20fb91abdf698e709c
G <0020992@gmail.com>
2023-12-13 23:38:33 +0300

    hw 20 2 # 2

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,5 +6,5 @@ app_name = 'catalog'
 urlpatterns = [
     path('', views.home, name='home'),
     path('contacts/', views.contact, name='contacts'),
-    path('product_detail/<int:pk>', views.product_detail, name='product_detail')
+    path('product/<int:pk>', views.product_detail, name='product_detail')
 ]
